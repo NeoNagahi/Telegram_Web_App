f7c14615fd9475deb2ae2aaa7a56ca7e93953ec9
NEO <142817981+NeoNagahi@users.noreply.github.com>
2023-12-02 18:11:54 +0300

    02-12-2023

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,11 +1,11 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "5153026043:AAEOA6Jgze21I8PGuZWKJoNMGY1Wzl6S5OI";
+const TOKEN = "6785362022:AAGjP55HEi_VeY1HDJVIGMTPwW4wMnQg6G4";
 const bot = new Telegraf(TOKEN);
 
 const web_link = "https://celebrated-torte-184681.netlify.app/";
 
 bot.start((ctx) =>
-  ctx.reply("Welcome :)))))", {
+  ctx.reply("Welcome :)))) @nameorder )", {
     reply_markup: {
       keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
     },
